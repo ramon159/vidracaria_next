d1952a9f7e325674969df94cab555382cdc12250
ramon159 <ramonarthur14@gmail.com>
2021-08-20 00:43:01 -0300

    Section Component: created

---
+++ src/components/Section/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export default styled.div`
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`
