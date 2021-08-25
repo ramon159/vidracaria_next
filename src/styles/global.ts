a765b74d89f568abae02404c69e5a19a99d8ea0b
ramon159 <ramonarthur14@gmail.com>
2021-08-24 21:44:02 -0300

    Swiper issue: fixed

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
-import { FONT, SIZE } from 'config/general'
+import { FONT, SIZE, COLOR } from 'config/general'
 
 export const GlobalStyles = createGlobalStyle`
   * {
@@ -41,4 +41,72 @@ export const GlobalStyles = createGlobalStyle`
       border: none;
       font-family: ${FONT.body};
   }
+
+  /* Swiper */
+
+  .swiper-container {
+  width: 100%;
+  height: 100%;
+
+}
+
+.swiper-slide {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.swiper-button-prev, .swiper-button-next{
+  background-color: ${COLOR.tertiary};
+  padding: 30px;
+  color: ${COLOR.white};
+}
+
+/* @media only screen and (min-width: 769px) {
+  .swiper-slide:first-child {
+    transition: transform 100ms;
+  }
+
+  .swiper-slide:first-child img {
+    transition: box-shadow 500ms;
+  }
+
+  .swiper-slide.swiper-slide-active:first-child {
+    transform: translateX(50%);
+    z-index: 2;
+  }
+
+  .swiper-slide.swiper-slide-active:first-child img {
+    box-shadow: 0px 32px 80px rgba(0, 0, 0, 0.35);
+  }
+
+  .swiper-slide:nth-child(2) {
+    transition: transform 100ms;
+  }
+
+  .swiper-slide.swiper-slide-next:nth-child(2) {
+    transform: translateX(55%);
+    z-index: 1;
+  }
+
+  .swiper-container[dir="rtl"] .swiper-slide.swiper-slide-active:first-child {
+    transform: translateX(-50%);
+  }
+
+  .swiper-container[dir="rtl"] .swiper-slide.swiper-slide-next:nth-child(2) {
+    transform: translateX(-55%);
+  }
+} */
+
+
 `
