71bd41ef80b48e9d70403645d69288467ebffdb1
ramon159 <ramonarthur14@gmail.com>
2021-09-01 16:05:45 -0300

    footer: clean

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,11 +3,7 @@ import * as S from './styles'
 function index() {
   return (
     <S.Footer>
-      <div className="footer__social">
-        <a href="https://www.instagram.com/speak.up.tech/"></a>
-        <a href="https://www.youtube.com/c/SpeakupTech"></a>
-      </div>
-      <p className="footer__copyright">Made with by Speak up Tech</p>
+      <div></div>
     </S.Footer>
   )
 }
