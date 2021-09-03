92d6594b8b082d73d6ca8f336f17b5ac025382c0
ramon159 <ramonarthur14@gmail.com>
2021-09-03 19:34:19 -0300

    test performance hero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Header from 'components/Header'
-import HeroSection from 'components/HeroSection'
+// import HeroSection from 'components/HeroSection'
 import ProductsSection from 'components/ProductsSection'
 import ContactSection from 'components/ContactSection'
 import WhatsappFloat from 'components/WhatsappFloat'
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <HeroSection />
+      {/* <HeroSection /> */}
       <ProductsSection />
       <ContactSection />
       <WhatsappFloat />
